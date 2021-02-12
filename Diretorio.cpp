c41d1e45973fca995c46cd50ebb7f7dc50fbe4b1
Anthony Lima <alima.als37@gmail.com>
2021-02-12 17:19:23 -0300

    sei la

--- Diretorio.cpp
+++ Diretorio.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 
+Diretorio::Diretorio(int n)
+{
+    criaDiretorio(n);
+}
+
+Diretorio::~Diretorio()
+{
+    
+}
+Diretorio::getDGlobal()
+{
+    return dGlobal;
+}
+Diretorio::setDGlobal(int n)
+{
+    dGlobal = n;
+}
+Diretorio::criaDiretorio(int n)
+{
+      for(int i = 0; i <(pow(n,2)), i++)
+      {
+          No *p = new No();
+      }
+}
 
 
 
